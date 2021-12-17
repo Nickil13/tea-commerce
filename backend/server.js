c51171d7e628e0ecaaaf607a7d80bceec35a6376
Nicki Lindstrom <lindstromn13@gmail.com>
2021-12-16 17:00:24 -0700

    pre-heroku

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,13 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 app.use(express.json({limit: '50mb'}));
 
 
---
+++ client/src/components/AdminBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function AdminBar() {
+    return (
+        <div className="admin-bar">
+            <div className="admin-links">
+                <Link className="btn btn-primary" to="/admin/orders">Orders</Link>
+                <Link className="btn btn-primary" to="/admin/users"> Users</Link>
+                <Link className="btn btn-primary" to="/admin/products">Products</Link>
+            </div> 
+        </div>
+    )
+}
